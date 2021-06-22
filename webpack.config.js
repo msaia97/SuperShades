042e9012346425fbe06ec7e4c5fa9945a44b536f
msaia97 <masonsaia@gmail.com>
2021-06-21 17:52:24 -0700

    Basic setup for proj

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const config = {
+  entry: path.join(__dirname, "./app.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "./public"),
+  },
+  plugins: [new webpack.ProgressPlugin()],
+  devtool: "source-map",
+};
+
+module.exports = config;
