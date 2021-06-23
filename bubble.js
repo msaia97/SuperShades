f00c4ef77bfeb44193d8dfa9640135717323e84b
msaia97 <masonsaia@gmail.com>
2021-06-23 16:51:37 -0700

    Got some stuff to render

--- bubble.js
+++ bubble.js
@@ -1,18 +1,710 @@
+let margin = 20,
+  diameter = 960,
+  colors = [
+    "White",
+    "Brown",
+    "Yellow",
+    "Purple",
+    "Silver/Gray",
+    "Black",
+    "Red",
+    "Blue",
+    "Green",
+  ];
 
-let width = 800,
-  height = 600;
-
-let canvas = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .append("g")
-  .attr("transform", "translate(50,50)");
+function color(){}
+// d3.scale
+//   .linear()
+//   .domain([-1, 1])
+//   .range(["red", "yellow", "green", "black", "purple"])
+  // .interpolate(d3.interpolateHcl);
 
 let pack = d3.layout
   .pack()
-  .size([width, height - 50])
-  .padding(10);
+  .padding(2)
+  .size([diameter - margin, diameter - margin])
+  .value(function (data) {
+    return data.size;
+  });
 
-d3.json("data.json");
+let svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", diameter)
+  .attr("height", diameter)
+  .append("g")
+  .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+let root = getData();
+let mainColors = root.children
+console.log(mainColors)
+
+
+let focus = root,
+  nodes = pack.nodes(root),
+  view;
+
+  // console.log(focus)
+
+let circle = svg
+  .selectAll("circle")
+  .data(nodes)
+  .enter()
+  .append("circle")
+  .attr("class", function (data) {
+    // console.log(data.children)
+    if(data.parent){
+      if(data.children){
+        return "main colors"
+      }else{
+        return "supers"
+      }
+    }else{
+      return "node root"
+    }
+  })
+  // .style("fill", function (data) {
+  //   let keys = Object.keys(data)
+  //   let val = Object.values(data)
+  //   let colors = ["White", "Brown", "Yellow", "Purple", "Silver/Gray", "Black", "Red", "Blue", "Green"]
+  //   // console.log(val[0])
+  //   val.forEach(node => {
+  //     // console.log(node)
+  //     if(colors.includes(node)){
+  //       console.log(node)
+  //       return node.style("fill" )
+  //     }
+  //   })
+
+  //   // return data.children ? color(data.depth) : null;
+  // })
+  .on("click", function (data) {
+    if (focus !== data) zoom(data), d3.event.stopPropagation();
+  });
+
+  let nodeRoot = svg
+    .selectAll("circle.root")
+    .style("fill", "red")
+
+  let colorCategory = svg
+    .selectAll("circle.main")
+    .style("fill", "yellow")
+
+  let superCol = svg
+    .selectAll("circle.supers")
+    .style("fill", "white")
+
+  console.log(circle)
+
+  circle.forEach(x => {
+    console.log(x)
+  })
+
+let text = svg
+  .selectAll("text")
+  .data(nodes)
+  .enter()
+  .append("text")
+  .attr("class", "label")
+  .style("fill-opacity", function (data) {
+    return data.parent === root ? 1 : 0;
+  })
+  .style("display", function (data) {
+    return data.parent === root ? null : "none";
+  })
+  .text(function (data) {
+    return data.name;
+  });
+
+  // console.log(text);
+let node = svg.selectAll("circle,text");
+
+// console.log(node[0]);
+// console.log(node[0][73]);
+// console.log()
+
+d3.select("body")
+  // .style("background", color(-1))
+  .style("background", color(-1))
+  .on("click", function () {
+    zoom(root);
+  });
+
+
+function getData(){
+  return {
+    name: "Super Shades",
+    value: 100,
+    children:
+     [
+      {
+        name: "Red",
+        value: 50,
+        children: [
+          {
+            name: "Ant Man",
+            main_color: "red",
+            second_color: "silver",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Daredevil",
+            main_color: "red",
+            second_color: "",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Deadpool",
+            main_color: "red",
+            second_color: "black",
+            third_color: "white",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Human Torch",
+            main_color: "red",
+            second_color: "blue",
+            third_color: "white",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Iron Man",
+            main_color: "red",
+            second_color: "yellow",
+            third_color: "silver",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Juggernaut",
+            main_color: "red",
+            second_color: "",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Magneto",
+            main_color: "red",
+            second_color: "purple",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Nebula",
+            main_color: "red",
+            second_color: "",
+            third_color: "blue",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Nightcrawler",
+            main_color: "red",
+            second_color: "blue",
+            third_color: "black",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Red Hulk",
+            main_color: "red",
+            second_color: "",
+            third_color: "blue",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Scarlet Witch",
+            main_color: "red",
+            second_color: "",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Spider-Man",
+            main_color: "red",
+            second_color: "blue",
+            third_color: "black",
+            type: "hero",
+            size: 20,
+          },
+        ],
+      },
+
+      {
+        name: "Yellow",
+        value: 50,
+        children: [
+          {
+            name: "Ancient One",
+            main_color: "yellow",
+            second_color: "purple",
+            third_color: "blue",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Rogue",
+            main_color: "yellow",
+            second_color: "green",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Wolverine",
+            main_color: "yellow",
+            second_color: "blue",
+            third_color: "black",
+            type: "hero",
+            size: 20,
+          },
+        ],
+      },
+
+      {
+        name: "Green",
+        value: 50,
+        children: [
+          {
+            name: "Abomination",
+            main_color: "green",
+            second_color: "",
+            third_color: "black",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Doctor Doom",
+            main_color: "green",
+            second_color: "gray",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Drax",
+            main_color: "green",
+            second_color: "red",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Electro",
+            main_color: "green",
+            second_color: "yellow",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Gamora",
+            main_color: "green",
+            second_color: "black",
+            third_color: "purple",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Green Goblin",
+            main_color: "green",
+            second_color: "purple",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Hulk",
+            main_color: "green",
+            second_color: "purple",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Iron Fist",
+            main_color: "green",
+            second_color: "yellow",
+            third_color: "black",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Lizard",
+            main_color: "green",
+            second_color: "white",
+            third_color: "purple",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Loki",
+            main_color: "green",
+            second_color: "yellow",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Mysterio",
+            main_color: "green",
+            second_color: "purple",
+            third_color: "yellow",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Phoenix",
+            main_color: "green",
+            second_color: "yellow",
+            third_color: "red",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Proffessor X",
+            main_color: "green",
+            second_color: "",
+            third_color: "silver",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "She Hulk",
+            main_color: "green",
+            second_color: "white",
+            third_color: "purple",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Vision",
+            main_color: "green",
+            second_color: "yellow",
+            third_color: "purple",
+            type: "hero",
+            size: 20,
+          },
+        ],
+      },
+
+      {
+        name: "Blue",
+        value: 50,
+        children: [
+          {
+            name: "Apocalypse",
+            main_color: "blue",
+            second_color: "red",
+            third_color: "dark blue",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Captain America",
+            main_color: "blue",
+            second_color: "red",
+            third_color: "white",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Captain Marvel",
+            main_color: "blue",
+            second_color: "red",
+            third_color: "yellow",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Cyclops",
+            main_color: "blue",
+            second_color: "yellow",
+            third_color: "red",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Beast",
+            main_color: "blue",
+            second_color: "yellow",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Doctor Strange",
+            main_color: "blue",
+            second_color: "red",
+            third_color: "yellow",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Iceman",
+            main_color: "blue",
+            second_color: "",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Mister Fantastic",
+            main_color: "blue",
+            second_color: "white",
+            third_color: "black",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Mystique",
+            main_color: "blue",
+            second_color: "red",
+            third_color: "yellow",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Quicksilver",
+            main_color: "blue",
+            second_color: "silver",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Thanos",
+            main_color: "blue",
+            second_color: "yellow",
+            third_color: "purple",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Thor",
+            main_color: "blue",
+            second_color: "red",
+            third_color: "yellow",
+            type: "hero",
+            size: 20,
+          },
+        ],
+      },
+
+      {
+        name: "Black",
+        value: 50,
+        children: [
+          {
+            name: "Black Cat",
+            main_color: "black",
+            second_color: "white",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Black Panther",
+            main_color: "black",
+            second_color: "",
+            third_color: "yellow",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Black Widow",
+            main_color: "black",
+            second_color: "",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Deathstroke",
+            main_color: "black",
+            second_color: "orange",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Nick Fury",
+            main_color: "black",
+            second_color: "",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Ghost Rider",
+            main_color: "black",
+            second_color: "red",
+            third_color: "white",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Punisher",
+            main_color: "black",
+            second_color: "white",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+
+          {
+            name: "Red Skull",
+            main_color: "black",
+            second_color: "",
+            third_color: "red",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Storm",
+            main_color: "black",
+            second_color: "yellow",
+            third_color: "white",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Venom",
+            main_color: "black",
+            second_color: "white",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+        ],
+      },
+
+      {
+        name: "White",
+        value: 50,
+        children: [
+          {
+            name: "Angel",
+            main_color: "white",
+            second_color: "blue",
+            third_color: "yellow",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Kingpin",
+            main_color: "white",
+            second_color: "purple",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+        ],
+      },
+
+      {
+        name: "Purple",
+        value: 50,
+        children: [
+          {
+            name: "Baron Zemo",
+            main_color: "purple",
+            second_color: "black",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Galactus",
+            main_color: "purple",
+            second_color: "blue",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Hawkeye",
+            main_color: "purple",
+            second_color: "black",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+        ],
+      },
+
+      {
+        name: "Silver/Gray",
+        value: 50,
+        children: [
+          {
+            name: "Colossus",
+            main_color: "gray",
+            second_color: "red",
+            third_color: "yellow",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Rhino",
+            main_color: "gray",
+            second_color: "",
+            third_color: "",
+            type: "villain",
+            size: 20,
+          },
+          {
+            name: "Silver Surfer",
+            main_color: "silver",
+            second_color: "",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "War Machine",
+            main_color: "silver",
+            second_color: "black",
+            third_color: "",
+            type: "hero",
+            size: 20,
+          },
+        ],
+      },
+
+      {
+        name: "Brown",
+        value: 50,
+        children: [
+          {
+            name: "Groot",
+            main_color: "brown",
+            second_color: "",
+            third_color: "green",
+            type: "hero",
+            size: 20,
+          },
+          {
+            name: "Thing",
+            main_color: "brown",
+            second_color: "blue",
+            third_color: "white",
+            type: "hero",
+            size: 20,
+          },
+        ],
+      },
+    ],
+  }; // end of object
+} //end of function
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require("path");
-const webpack = require("webpack");
+// const path = require("path");
+// const webpack = require("webpack");
 
-const config = {
-  entry: path.join(__dirname, "./public/index.html"),
-  output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "./public"),
-  },
-  plugins: [new webpack.ProgressPlugin()],
-  devtool: "source-map",
-};
+// const config = {
+//   entry: path.join(__dirname, "./public/index.html"),
+//   output: {
+//     filename: "bundle.js",
+//     path: path.join(__dirname, "./public"),
+//   },
+//   plugins: [new webpack.ProgressPlugin()],
+//   devtool: "source-map",
+// };
 
-module.exports = config;
+// module.exports = config;
